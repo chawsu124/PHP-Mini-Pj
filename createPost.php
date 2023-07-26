be6548085d2c1ac5de167818cf6a5d010c8cc382
chawsu124 <113336579+chawsu124@users.noreply.github.com>
2023-07-26 18:58:52 +0630

    Update createPost.php

--- createPost.php
+++ createPost.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic PHP CRUD by Sr.YeMyintSoe</title>
+    <title>Basic PHP CRUD</title>
     <link rel="stylesheet" href="sources/css/bootstrap.min.css">
 </head>
 <body class="bg-white">
