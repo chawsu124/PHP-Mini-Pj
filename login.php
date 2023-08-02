131d987b32ed81d309f94b2db2f0b5df1d805870
chawsu124 <113336579+chawsu124@users.noreply.github.com>
2023-08-03 00:23:34 +0630

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Basic Login by Sr.YeMyintSoe</title>
+    <title>PHP Basic Login</title>
     <link rel="stylesheet" href="sources/css/bootstrap.min.css">
 </head>
 <body>
